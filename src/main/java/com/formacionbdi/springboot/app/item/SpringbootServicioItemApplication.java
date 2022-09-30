3b8ea9f3f06f4896fb79f2d379a1b556d14dfeb4
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-09-29 19:19:58 -0500

    Se añade Hystrix para uso de tolerancia a fallos

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,9 +2,11 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 //Se quita anotación @RibbonClient porque ya se usará Eureka
 //@RibbonClient(name="servicio-productos")
--- src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.model.Item;
+import com.formacionbdi.springboot.app.item.model.Producto;
 import com.formacionbdi.springboot.app.item.service.IItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -23,10 +25,25 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod="metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 		
+	}
+	
+	//Es el método alternativo para evitar errores, Hystrix lo va a llamar en caso de excepcion
+	//Se podria llamar a otros microservicios también
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Soni");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
 	}
 
 }
